4d33dad502998bbddd3ad72b28ada5a3e0088075
Marcos Teruo Ouchi <marcos.teruo.ouchi@gmail.com>
2017-07-04 15:50:40 -0300

    inclusão de token

--- scenetojson.py
+++ scenetojson.py
@@ -2,10 +2,10 @@ import Algorithmia #Importa a Biblioteca Algorithmia
 
 input = {
     "video": "url",
-    "detector": "content"
+    "detector": "content",
     "output_collection_frames": "scenecuts"
 }
 
-client = Algorithmia.client('token')
+client = Algorithmia.client('sim0K+aJ2N+cJ+pP75A+dffnZGU1')
 algo = client.algo('media/SceneDetection/0.1.6')
 print algo.pipe(input)
