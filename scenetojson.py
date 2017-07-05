172f6dade09279d53fa6231535fe30fc2bdc94f2
Marcos Teruo Ouchi <marcos.teruo.ouchi@gmail.com>
2017-07-04 21:00:36 -0300

    inserção dos parâmetros no arquivo scenetojason.py e criação da pasta img

--- scenetojson.py
+++ scenetojson.py
@@ -1,11 +1,11 @@
 import Algorithmia #Importa a Biblioteca Algorithmia
 
 input = {
-    "video": "url",
+    "video": "https://www.youtube.com/watch?v=x_EZZrIErDI&t=17s",
     "detector": "content",
     "output_collection_frames": "scenecuts"
 }
 
 client = Algorithmia.client('sim0K+aJ2N+cJ+pP75A+dffnZGU1')
 algo = client.algo('media/SceneDetection/0.1.6')
-print algo.pipe(input)
+print (algo.pipe(input))
